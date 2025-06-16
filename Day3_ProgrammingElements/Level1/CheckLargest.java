a4969e916e24b7ada2ba5ef64fc65177ffa5c368
AyushiAgrawa <gagan.singh@example.com>
2025-06-16 21:49:08 +0530

    Add Level1

---
+++ Day3_ProgrammingElements/Level1/CheckLargest.java
@@ -0,0 +1,27 @@
+package Day3_ProgrammingElements;
+import java.util.Scanner;
+public class CheckLargest {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter number1: ");
+		int a = sc.nextInt();
+		System.out.println("Enter number2: ");
+		int b = sc.nextInt();
+		System.out.println("Enter number3: ");
+		int c = sc.nextInt();
+		 
+		if(a > b && a > c) {
+			System.out.println("Number1 is largest");
+		}
+		else if(b > a && b > c){
+			System.out.println("Number2 is largest");
+		}else {
+			System.out.println("Number3 is largest");
+		}
+
+	}
+
+	
+
+}
---
+++ Day3_ProgrammingElements/Level1/ForLoopForRocketLaunch.java
@@ -0,0 +1,10 @@
+package Day3_ProgrammingElements;
+
+public class ForLoopForRocketLaunch {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
